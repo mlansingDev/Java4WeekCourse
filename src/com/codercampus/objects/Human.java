0cc9b5f5767587f24d28aa342ed159b588ff0d30
DESKTOP-33P4Q1T\mlans <michaellansing88@gmail.com>
2022-09-13 15:14:46 -0400

    1st push for week 3.

---
+++ src/com/codercampus/objects/Human.java
@@ -0,0 +1,8 @@
+package com.codercampus.objects;
+
+public class Human {
+	String eyeColor;
+	String height;
+	String weight;
+	String gender;
+}
---
+++ src/com/codercampus/objects/RunnableClass.java
@@ -0,0 +1,11 @@
+package com.codercampus.objects;
+
+public class RunnableClass {
+
+	public static void main(String[] args) {
+		// Class -> Blueprint for an Object
+		// Object -> Noun, it's an  instantiation of a Class
+		
+		Human mikeLansing = new Human();
+	}
+}
