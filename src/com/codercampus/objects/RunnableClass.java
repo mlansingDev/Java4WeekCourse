d900cfef26d95298e1ab9915292d9b84e8178e4c
DESKTOP-33P4Q1T\mlans <michaellansing88@gmail.com>
2022-09-13 15:35:15 -0400

    dot properties lesson

--- src/com/codercampus/objects/RunnableClass.java
+++ src/com/codercampus/objects/RunnableClass.java
@@ -7,5 +7,19 @@ public class RunnableClass {
 		// Object -> Noun, it's an  instantiation of a Class
 		
 		Human mikeLansing = new Human();
+		mikeLansing.eyeColor = "Hazel";
+		mikeLansing.gender = "Male";
+		mikeLansing.height = "6'3\"";
+		mikeLansing.weight = "275";
+		
+		System.out.println(mikeLansing);
+		
+		Human elonMusk = new Human();
+		elonMusk.eyeColor = "Blue";
+		elonMusk.gender = "Male";
+		elonMusk.height = "6'1\"";
+		elonMusk.weight = "190";
+		
+		System.out.println(elonMusk);
 	}
 }
